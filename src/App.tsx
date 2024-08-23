cee0cb6fa756d28c418d70260653b4204944dbe3
martin0123478 <martin_agui97@outlook.es>
2024-08-23 13:33:28 -0600

    feat:create form and list components

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import PatientForm from "./components/PatientForm"
+import { PatientList } from "./components/PatientList"
 
 
 function App() {
@@ -5,7 +7,15 @@ function App() {
 
   return (
     <>
-      <h1 className="text-4xl">Pacientes</h1>
+      <div className="container mx-auto mt-20">
+        <h1 className="font-black text-5xl text-center md:2/3 md:mx-auto">Seguimiento de pacientes{''}
+          <span className="text-indigo-700"> Veterinaria</span></h1>
+
+        <div className="mt-12 md:flex">
+          <PatientForm />
+          <PatientList />
+        </div>
+      </div>
     </>
   )
 }
